1daf061ee6ac21272941f26948485b94e83967a5
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-08-04 09:42:54 -0300

    [feat]: create route book editing feature

--- src/routes/books.js
+++ src/routes/books.js
@@ -4,20 +4,45 @@ const errorMessages = require('../functions/errorMesages');
 const bookSchema = require('../models/book');
 const router = express.Router();
 
-/* GET users listing. */
-router.post('/', DBconnect, async function(req, res) {
+/* GET book listing. */
+router.post('/', DBconnect, async function (req, res) {
   try {
     //#swagger.tags = ['Livro']
-    let { titulo, num_paginas, isbn, editora} = req.body;
-    const BDanswer = await bookSchema.create({titulo, num_paginas, isbn, editora})
+    let { titulo, num_paginas, isbn, editora } = req.body;
+    const DBanswer = await bookSchema.create({ titulo, num_paginas, isbn, editora })
 
     res.status(200).json({
       status: "ok",
       statusMensagem: "Livro Criado com sucesso",
-      resposta: BDanswer,
+      resposta: DBanswer,
     })
-  } catch(error){
-    return errorMessages(res,error);
+  } catch (error) {
+    return errorMessages(res, error);
+  }
+});
+
+router.put('/:id', DBconnect, async function (req, res) {
+  try {
+    //#swagger.tags = ['Livro']
+    let bookID = req.params.id;
+    let { titulo, num_paginas, isbn, editora } = req.body;
+    const bookExists = await bookSchema.find({ _id: bookID });
+    if (!bookExists) {
+      throw new Error("Livro não encontrado.");
+    }
+    const DBanswer = await bookSchema.updateOne({ _id: bookID }, { titulo, num_paginas, isbn, editora });
+
+    if (DBanswer?.modifiedCount > 0) {
+      const bookData = await bookSchema.findOne({ _id: bookID });
+      res.status(200).json({
+        status: "ok",
+        statusMensagem: "Livro editado com sucesso",
+        resposta: bookData,
+      })
+    }
+
+  } catch (error) {
+    return errorMessages(res, error);
   }
 });
 
