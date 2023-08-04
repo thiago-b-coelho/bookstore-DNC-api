d86fda812ebd92636121423774ed56129b310d16
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-08-04 17:48:23 -0300

    [fix]: fix bug causing axios to crash

--- src/routes/books.js
+++ src/routes/books.js
@@ -5,7 +5,7 @@ const bookSchema = require('../models/book');
 const router = express.Router();
 
 /* GET book listing. */
-router.post('/', DBconnect, async function (req, res) {
+router.post('/cadastro', DBconnect, async function (req, res) {
   try {
     //#swagger.tags = ['Livro']
     let { titulo, num_paginas, isbn, editora } = req.body;
@@ -21,12 +21,13 @@ router.post('/', DBconnect, async function (req, res) {
   }
 });
 
-router.put('/:id', DBconnect, async function (req, res) {
+router.put('/edicao/:id', DBconnect, async function (req, res) {
   try {
     //#swagger.tags = ['Livro']
+    //#swagger.description = "Criação de livros"
     let bookID = req.params.id;
     let { titulo, num_paginas, isbn, editora } = req.body;
-    const bookExists = await bookSchema.find({ _id: bookID });
+    const bookExists = await bookSchema.findOne({ _id: bookID });
     if (!bookExists) {
       throw new Error("Livro não encontrado.");
     }
@@ -41,6 +42,62 @@ router.put('/:id', DBconnect, async function (req, res) {
       })
     }
 
+  } catch (error) {
+    return errorMessages(res, error);
+  }
+});
+
+router.get('/', DBconnect, async function (req, res) {
+  try {
+    //#swagger.tags = ['Livro']
+    //#swagger.description = "Edição de livros"
+
+    const DBanswer = await bookSchema.find();
+
+    res.status(200).json({
+      status: "ok",
+      statusMensagem: "Livros listados com sucesso",
+      resposta: DBanswer,
+    })
+  } catch (error) {
+    return errorMessages(res, error);
+  }
+});
+
+router.get('/:id', DBconnect, async function (req, res) {
+  try {
+    //#swagger.tags = ['Livro']
+    //#swagger.description = "Edição de livros"
+    const bookID = req.params.id;
+    const DBanswer = await bookSchema.findOne({_id: bookID});
+
+    res.status(200).json({
+      status: "ok",
+      statusMensagem: "Livros listados com sucesso",
+      resposta: DBanswer,
+    })
+  } catch (error) {
+    return errorMessages(res, error);
+  }
+});
+
+router.delete('/:id', DBconnect, async function (req, res) {
+  try {
+    //#swagger.tags = ['Livro']
+    //#swagger.description = "Remoção de livros"
+    let bookID = req.params.id;
+    const bookExists = await bookSchema.findOne({ _id: bookID });
+    if (!bookExists) {
+      throw new Error("Livro não encontrado.");
+    }
+
+    const DBanswer = await bookSchema.deleteOne({_id: bookID});
+
+    res.status(200).json({
+      status: "ok",
+      statusMensagem: "Livro deletado com sucesso",
+      resposta: DBanswer,
+    })
   } catch (error) {
     return errorMessages(res, error);
   }
