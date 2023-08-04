96f048b9ccb41c56a77e67c6aba87423603b48e6
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-08-04 09:24:35 -0300

    [feat]: create route to add book to DB

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const DBconnect = require('../middlewares/DBconnect');
 const errorMessages = require('../functions/errorMesages');
+const bookSchema = require('../models/book');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', DBconnect, function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', DBconnect, async function(req, res) {
+  try {
+    //#swagger.tags = ['Livro']
+    let { titulo, num_paginas, isbn, editora} = req.body;
+    const BDanswer = await bookSchema.create({titulo, num_paginas, isbn, editora})
+
+    res.status(200).json({
+      status: "ok",
+      statusMensagem: "Livro Criado com sucesso",
+      resposta: BDanswer,
+    })
+  } catch(error){
+    return errorMessages(res,error);
+  }
 });
 
 module.exports = router;
