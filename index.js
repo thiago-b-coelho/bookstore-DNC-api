5e9cc7ab781ed5d29827e6a610c17eec56b0f4d7
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-08-04 21:04:45 -0300

    [fix]: allow cors

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 if (process.env.NODE_ENV !== 'test') {
     const swaggerFile = require('./swagger/swagger_output.json');
